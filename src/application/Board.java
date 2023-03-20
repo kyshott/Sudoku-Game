0b2a5f0be8f1e3eada5ff35641b5286c1ea1c0d1
Kyle <Kyle@DESKTOP-M2T84DD>
2023-03-20 08:12:07 -0400

    Added some test code for the user input buttons

--- src/application/Board.java
+++ src/application/Board.java
@@ -1,6 +1,9 @@
 package application;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -30,6 +33,56 @@ public class Board extends Application{
 		
 		
 		
+		
+	}
+	
+	public void buttonTest(Stage primaryStage) { // More testing using the "gridpane" class to set buttons objects in a grid + some cosmetic experiments
+		
+		/*
+		 * 	primaryStage.setTitle("Sudoku Solver");
+    
+    Button button1 = new Button("1");
+    Button button2 = new Button("2");
+    Button button3 = new Button("3");
+    Button button4 = new Button("4");
+    Button button5 = new Button("5");
+    Button button6 = new Button("6");
+    Button button7 = new Button("7");
+    Button button8 = new Button("8");
+    Button button9 = new Button("9");
+    
+    button1.setStyle("-fx-border-color: #ff0000; -fx-border-width: 2px;");
+    button2.setStyle("-fx-border-color: #ff0000; -fx-border-width: 2px;");
+    button3.setStyle("-fx-border-color: #ff0000; -fx-border-width: 2px;");
+    button4.setStyle("-fx-border-color: #ff0000; -fx-border-width: 2px;");
+    button5.setStyle("-fx-border-color: #ff0000; -fx-border-width: 2px;");
+    button6.setStyle("-fx-border-color: #ff0000; -fx-border-width: 2px;");
+    button7.setStyle("-fx-border-color: #ff0000; -fx-border-width: 2px;");
+    button8.setStyle("-fx-border-color: #ff0000; -fx-border-width: 2px;");
+    button9.setStyle("-fx-border-color: #ff0000; -fx-border-width: 2px;");
+    
+    GridPane gridPane = new GridPane();
+    
+    gridPane.setAlignment(Pos.BOTTOM_RIGHT);
+    
+    gridPane.add(button1, 0, 0);
+    gridPane.add(button2, 1, 0);
+    gridPane.add(button3, 2, 0);
+    gridPane.add(button4, 0, 1);
+    gridPane.add(button5, 1, 1);
+    gridPane.add(button6, 2, 1);
+    gridPane.add(button7, 0, 2);
+    gridPane.add(button8, 1, 2);
+    gridPane.add(button9, 2, 2);
+    
+    Scene scene = new Scene(gridPane, 800, 500);
+        
+    primaryStage.setScene(scene);
+    
+    primaryStage.setResizable(false);
+    
+    primaryStage.show();
+		 */
 		
 	}
 
