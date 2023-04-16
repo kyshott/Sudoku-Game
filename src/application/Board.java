e200390c898cda7b0e1295cadcb698feed8e4dbf
Kyle <Kyle@DESKTOP-M2T84DD>
2023-04-16 15:57:59 -0400

    Random Board generation complete.
    Number of solved cells in the random generation can be changed by
    editing the "17" number in the "Board" class. Will add start button and
    timer soon.

--- src/application/Board.java
+++ src/application/Board.java
@@ -2,52 +2,122 @@ package application;
 
 
 public class Board extends Solver{
-	//build board with player and solutions here
-	//LOTS OF UNUSED METHODS THAT MIGHT BE HELPFUL FOR GENERATING SOLVABLE BOARD
+	
+	public int[][] generateBoard() {
+	   
+	    int[][] board = new int[9][9];
+	    for (int i = 0; i < 9; i++) {
+	        for (int j = 0; j < 9; j++) {
+	            board[i][j] = 0;
+	        }
+	    }
+
+	    
+	    int count = 0;
+	    while (count < 17) {
+	       
+	        int row = (int) (Math.random() * 9);
+	        int col = (int) (Math.random() * 9);
+
+	        
+	        if (board[row][col] != 0) {
+	        	
+	            continue;
+	            
+	        }
+
+	       
+	        int val = (int) (Math.random() * 9) + 1;
+	        
+	        if (isPossible(board, row, col, val)) {
+	        	
+	            board[row][col] = val;
+	            
+	            count++;
+	            
+	        }
+	        
+	    }
+	    
+	    return board;
+	}
+
+
+	public static void printtempBoard(int[][] board) {
+	    for (int i = 0; i < 9; i++) {
+	        for (int j = 0; j < 9; j++) {
+	            System.out.print(board[i][j] + " ");
+	        }
+	        System.out.println();
+	    }
+	}
+
+	
+	public static boolean isPossible(int[][] board, int row, int col, int val) {
+	   
+	    for (int i = 0; i < 9; i++) {
+	    	
+	        if (board[row][i] == val || board[i][col] == val) {
+	        	
+	            return false;
+	        }
+	    }
+
+	    int subRow = (row / 3) * 3;
+	    
+	    int subCol = (col / 3) * 3;
+	    
+	    for (int i = subRow; i < subRow + 3; i++) {
+	    	
+	        for (int j = subCol; j < subCol + 3; j++) {
+	        	
+	            if (board[i][j] == val) {
+	            	
+	                return false;
+	                
+	            }
+	        }
+	    }
+
+	    return true;
+	}
+	
+	
 	private final int GRID_SIZE = 9;
 	private int[][] sol;
 	private int[][] init;
 	private int[][] player;
 	
 	public Board() {
-		sol = new int[][] 
-		{
-			{7,3,2,4,5,8,6,1,9},
-			{9,5,6,1,7,3,8,2,4},
-			{1,8,4,6,2,9,5,3,7},
-			{8,7,1,5,6,4,3,9,2},
-			{6,4,3,8,9,2,7,5,1},
-			{2,9,5,3,1,7,4,6,8},
-			{3,2,9,7,8,6,1,4,5},
-			{4,1,8,2,3,5,9,7,6},
-			{5,6,7,9,4,1,2,8,3}
-			};
-			//INIT BOARD ONLY NEEDS 3 IN BOTTOM F0R TESTING PURPOSE. REAL INITIAL BOARD COMMENTED OUT BELOW
-			init = new int[][]
-			{
-				{7,3,2,4,5,8,6,1,9},
-				{9,5,6,1,7,3,8,2,4},
-				{1,8,4,6,2,9,5,3,7},
-				{8,7,1,5,6,4,3,9,2},
-				{6,4,3,8,9,2,7,5,1},
-				{2,9,5,3,1,7,4,6,8},
-				{3,2,9,7,8,6,1,4,5},
-				{4,1,8,2,3,5,9,7,6},
-				{5,6,7,9,4,1,2,8,0}
-				};
-				/*
-				{7,0,2,0,5,0,6,0,0},
-				{0,0,0,0,0,3,0,0,0},
-				{1,0,0,0,0,9,5,0,0},
-				{8,0,0,0,0,0,0,9,0},
-				{0,4,3,0,0,0,7,5,0},
-				{0,9,0,0,0,0,0,0,8},
-				{0,0,9,7,0,0,0,0,5},
-				{0,0,0,2,0,0,0,0,0},
-				{0,0,7,0,4,0,2,0,3}
-				}
-				*/
+		
+	final int[][] initBoard = generateBoard();
+	
+    int[][] solveBoard = new int[9][9];
+    
+    for (int row = 0; row < 9; row++) {
+    	
+        for (int col = 0; col < 9; col++) {
+        	
+            solveBoard[row][col] = initBoard[row][col];
+            
+        }
+        
+    }
+	
+	printtempBoard(initBoard);
+	
+	solve(solveBoard);
+	
+	System.out.println();
+	
+	printtempBoard(solveBoard);
+		
+		sol = solveBoard;
+		
+		init = initBoard;
+				
 				player = new int[9][9];
+				
 	}
 	
 	public int[][] getSol() {
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,4 @@
-package application;
-	
+package application;	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,37 +7,51 @@ import javafx.scene.*;
 
 
 public class Main extends Application {
+	
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws Exception {
+		
+		/*
+		
 		//TESTING BOARD. NEEDS TO BE OUTPUTED TO THE GUI AND NOT CONSOLE
-		try {
-				int[][] testBoard = {
-						{7,0,2,0,5,0,6,0,0},
-						{0,0,0,0,0,3,0,0,0},
-						{1,0,0,0,0,9,5,0,0},
-						{8,0,0,0,0,0,0,9,0},
-						{0,4,3,0,0,0,7,5,0},
-						{0,9,0,0,0,0,0,0,8},
-						{0,0,9,7,0,0,0,0,5},
-						{0,0,0,2,0,0,0,0,0},
-						{0,0,7,0,4,0,2,0,3}
-						};
-				Solver solver = new Solver();
-				solver.solve(testBoard);
-				solver.printBoard(testBoard);
-				System.out.println();
-				
-				
+				try {
+						int[][] testBoard = {
+								{7,0,2,0,5,0,6,0,0},
+								{0,0,0,0,0,3,0,0,0},
+								{1,0,0,0,0,9,5,0,0},
+								{8,0,0,0,0,0,0,9,0},
+								{0,4,3,0,0,0,7,5,0},
+								{0,9,0,0,0,0,0,0,8},
+								{0,0,9,7,0,0,0,0,5},
+								{0,0,0,2,0,0,0,0,0},
+								{0,0,7,0,4,0,2,0,3}
+								};
+						Solver solver = new Solver();
+						solver.solve(testBoard);
+						solver.printBoard(testBoard);
+						System.out.println();
+						
+						
+					
+					//STARTING OF STAGE
+					Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+					Scene scene = new Scene(root, 720, 480);
+					primaryStage.setTitle("Sudoku Game");
+					primaryStage.setScene(scene);
+					primaryStage.show();
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
 			
-			//STARTING OF STAGE
-			Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
-			Scene scene = new Scene(root, 720, 480);
-			primaryStage.setTitle("Sudoku Game");
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		*/
+
+		Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+		Scene scene = new Scene(root, 720, 480);
+		primaryStage.setTitle("Sudoku Game");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
 	}
 	
 	
